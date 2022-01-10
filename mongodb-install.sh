952b8e116035d3029e659ad5414e9a368ebb3be2
ergün elvan bilsel <30519822+erelbi@users.noreply.github.com>
2022-01-10 09:11:47 +0300

    Update mongodb-install.sh

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -1,60 +1,168 @@
-# -*- mode: ruby -*
-# vi: set ft=ruby :
-# require 'getoptlong'
+INSTALL_DIR="/data"
+MONGODB_CONF_FILES=("mongodbPrimary.conf" "mongodbSecondary1.conf" "mongodbSecondary2.conf")
+MONGODB_NODES=("mongodbPrimary" "mongodbSecondary1" "mongodbSecondary2")
+MONGODB_PORTS=(27017 27018 27019)
+MONGODB_ROOT_USER="db_root"
+MONGODB_ROOT_PASSWORD="password"
+ID=$(egrep '^(ID)=' /etc/os-release)
+OS=${ID:3}
+REPO_FEDORA=/etc/yum.repos.d/mongodb-org-4.2.repo
 
-# opts = GetoptLong.new(
-#   [ '--custom-option', GetoptLong::OPTIONAL_ARGUMENT ]
-# )
+create_dir(){
+  mkdir -p "$INSTALL_DIR/mongo/db"
+  chmod  777 "$INSTALL_DIR/mongo/db"
+  mkdir -p "$INSTALL_DIR/mongo/logs"
+  mkdir -p "$INSTALL_DIR/mongo/conf"
+  mkdir -p "$INSTALL_DIR/db"
+  for i in "${MONGODB_NODES[@]}"
+  do
+  mkdir "$INSTALL_DIR/mongo/db/$i"
+  done
+}
 
-# customParameter=''
+setup_mongodb(){
 
-# opts.each do |opt, arg|
-#   case opt
-#     when '--custom-option'
-#       customParameter=arg
-#   end
-# end
-## Farklı ortamlar için hazırlanacak
-# if OS.windows?
-#     puts "Vagrant launched from windows."
-# elsif OS.mac?
-#     puts "Vagrant launched from mac."
-# elsif OS.unix?
-#     puts "Vagrant launched from unix."
-# elsif OS.linux?
-#     puts "Vagrant launched from linux."
-# else
-#     puts "Vagrant launched from unknown platform."
-# end
-Vagrant.configure("2") do |config|
-   ### only ubuntu2004 and fedora
+  if [[ $1 = fedora ]]
+  then
+   echo "install mongodb for fedora31"
+   cat <<EOF > $REPO_FEDORA
+[mongodb-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
+EOF
+   yum update && yum install -y mongodb-org mongodb-org-tools mongodb-org-shell
+   chown -R mongod:mongod  "$INSTALL_DIR"
+   
 
-   #config.vm.box = "generic/ubuntu2004"
-   config.vm.box = "generic/fedora31"
-   config.vm.provision "shell", path: "mongodb-install.sh"  
-   #config.vm.provision "shell", path: "mongodb-run.sh", run: "always"
-   config.vm.network "forwarded_port", guest: 27017, host: 27017
-   config.vm.network "forwarded_port", guest: 27018, host: 27018
-   config.vm.network "forwarded_port", guest: 27019, host: 27019
+  elif [[ $1 = ubuntu ]]
+  then
+  echo "install mongodb ubuntu20.04"
+  curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc |  apt-key add - &&
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+  apt update && apt install -y mongodb-org=4.4.1 mongodb-org-server=4.4.1 mongodb-org-shell=4.4.1 mongodb-org-mongos=4.4.1 mongodb-org-tools=4.4.1
+  chown -R mongodb:mongodb  "$INSTALL_DIR"
+  else
+  echo "Not Found Operation System"
+  exit 1
+  fi
+  
+}
 
-  ##Virtualbox
-  #  config.vm.provider "virtualbox" do |vb|
-  #   vb.name = 'mongodb-replicaset'    
-  #   vb.customize ["modifyvm", :id, "--cpuexecutioncap", "50"]
-  #   vb.memory = 4096
-  #   vb.cpus = 2
-  #  end
-  ##vmware
-  #  config.vm.provider "vmware_desktop" do |v|
-  #   v.vmx["memsize"] = "4096"
-  #   v.vmx["numvcpus"] = "2"
-  #  end
-  ## Libvirt
-   config.vm.provider :libvirt do |kvm|
-    kvm.memory = 4096
-    kvm.cpus = 2
-    # kvm.volume_cache = 'none'
-    # kvm.storage_pool_name = 'vagrant-mongodb'
-   end
 
-end
+configuration_data(){
+   
+
+    for i in "${MONGODB_CONF_FILES[@]}"
+    do	
+    	touch "$INSTALL_DIR/mongo/conf/$i"	
+    done
+
+    ITER=1
+    for i in "${MONGODB_NODES[@]}"
+    do
+    echo "$INSTALL_DIR/mongo/conf/$i.conf"
+    echo "
+    systemLog:
+      destination: file
+      path: $INSTALL_DIR/mongo/logs/$i.log
+      logAppend: true
+    storage:
+      dbPath: $INSTALL_DIR/mongo/db/$i
+      journal:
+        enabled: true
+    net:
+      port: ${MONGODB_PORTS[$ITER -1 ]}
+      bindIp: 0.0.0.0
+    " > "$INSTALL_DIR/mongo/conf/$i.conf"
+    ITER=$(expr $ITER + 1)
+    done
+    
+
+    
+}
+
+
+start_server(){
+        if [[ $1 = fedora ]]
+        then
+          chown -R mongod:mongod  "$INSTALL_DIR"
+          firewall-cmd --add-port=27017/tcp --permanent
+          firewall-cmd --reload
+          for i in "${MONGODB_CONF_FILES[@]}"
+          do
+            mongod --config "$INSTALL_DIR/mongo/conf/$i" --fork
+          done
+        elif [[ $1 = ubuntu ]]
+        then
+           chmod 777 "$INSTALL_DIR"/db
+           chown -R mongodb:mongodb  "$INSTALL_DIR"
+           for i in "${MONGODB_CONF_FILES[@]}"
+          do
+            mongod --config "$INSTALL_DIR/mongo/conf/$i" --fork
+          done
+        else
+          exit 1
+        fi
+
+
+}
+
+
+create_mongo_user(){
+    for i in "${MONGODB_PORTS[@]}"
+    do
+    mongo --port $i admin --eval 'db.createUser({user: "'"$MONGODB_ROOT_USER"'", pwd: "'"$MONGODB_ROOT_PASSWORD"'", roles: [ { role: "root", db: "admin" } ]})'
+    done
+}
+
+openssl_create_key(){
+  openssl rand -base64 756 > "$INSTALL_DIR/mongo/.mdbia"
+  chmod 400 "$INSTALL_DIR/mongo/.mdbia"
+}
+
+mongo_reconfigure(){
+    cd "$INSTALL_DIR/mongo/conf"
+    for i in *.conf
+    do 
+    echo "
+    security:
+      authorization: enabled
+      keyFile: $INSTALL_DIR/mongo/.mdbia
+    replication:
+      replSetName: rs1
+    " >> $i
+    done
+}
+
+reboot_mongodb(){
+for i in "${MONGODB_PORTS[@]}"
+do
+mongo --port $i admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); db.shutdownServer()'
+done
+
+for i in "${MONGODB_CONF_FILES[@]}"
+do
+  mongod --config "$INSTALL_DIR/mongo/conf/$i" --fork
+done
+}
+
+
+replica_Set_mongo(){
+mongo --port 27017 admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); rs.initiate({"_id":"rs1","members":[{"_id":1,"host":"127.0.0.1:27017"}]})'
+mongo --port 27017 admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); rs.add("127.0.0.1:27018")'
+mongo --port 27017 admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); rs.add("127.0.0.1:27019")'
+mongo --port 27017 admin --eval 'db.auth("'"$MONGODB_ROOT_USER"'", "'"$MONGODB_ROOT_PASSWORD"'"); cfg = rs.conf(); cfg.members[0].priority = 10; cfg.members[1].priority = 1; cfg.members[2].priority = 1; rs.reconfig(cfg)'
+}
+
+create_dir
+setup_mongodb $OS
+configuration_data
+start_server $OS
+create_mongo_user
+openssl_create_key
+mongo_reconfigure
+reboot_mongodb
+replica_Set_mongo
